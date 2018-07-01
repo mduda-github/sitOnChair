6bf96e4915b8c53f00f9fece6fbdbcb1676c80bf
Marcin Duda <marc.duda94@gmail.com>
2018-07-01 11:34:10 +0200

    Add functionality to choose chair section

--- js/app.js
+++ js/app.js
@@ -65,9 +65,67 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(prev);
     console.log(next);
 
-    // kalkulator
+    // Drop-down
 
-    var list1 = document.querySelector(".")
+    var lists = document.querySelectorAll(".panel-list");
+    var arrows = document.querySelectorAll(".panel-list");
+
+
+    var list1 = document.querySelectorAll(".panel-list")[0];
+    var list2 = document.querySelectorAll(".panel-list")[1];
+    var list3 = document.querySelectorAll(".panel-list")[2];
+
+    console.log(list1, list2, list3);
+
+    var arr1 = document.querySelectorAll(".list-arrow")[0];
+    var arr2 = document.querySelectorAll(".list-arrow")[1];
+    var arr3 = document.querySelectorAll(".list-arrow")[2];
+
+    console.log(arr1, arr2, arr3);
+
+    arr1.addEventListener("click", function () {
+        if (list1.style.visibility === "visible") {
+            list1.style.visibility = "hidden";
+            list1.style.opacity = "0";
+        } else {
+            list1.style.visibility = "visible";
+            list1.style.opacity = "1";
+        }
+    });
+
+    arr2.addEventListener("click", function () {
+        if (list2.style.visibility === "visible") {
+            list2.style.visibility = "hidden";
+            list2.style.opacity = "0";
+        } else {
+            list2.style.visibility = "visible";
+            list2.style.opacity = "1";
+        }
+    });
+
+    arr3.addEventListener("click", function () {
+        if (list3.style.visibility === "visible") {
+            list3.style.visibility = "hidden";
+            list3.style.opacity = "0";
+        } else {
+            list3.style.visibility = "visible";
+            list3.style.opacity = "1";
+        }
+    });
+
+
+    // Transport checkbox
+
+    var checkbox = document.querySelector(".checkbox");
+
+    checkbox.addEventListener("click", function () {
+        if (checkbox.style.backgroundImage === "") {
+            checkbox.style.backgroundImage = "url('./images/form_ok.jpg')";
+            checkbox.style.backgroundPosition = "center";
+            checkbox.style.backgroundRepeat = "no-repeat";
+        } else {
+            checkbox.style.backgroundImage = "";
+    }})
 
 
 });
